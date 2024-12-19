6569b7b6dd08bbbb6328e9a2ee4710631d67025e
ryanbuck182@ <ryanbuck182@gmail.com>
2024-12-18 19:40:57 -0500

    Complete day 4

---
+++ 2024/Day04/part1.py
@@ -0,0 +1,25 @@
+from re import findall, DOTALL
+
+# Part One
+# https://adventofcode.com/2024/day/4
+
+file = open("input.txt", "r")
+text = file.read()
+
+
+def match_count(regex: str) -> int:
+    regex = "(?=" + regex + ")"
+    return len(findall(regex, text, DOTALL))
+
+
+total = 0
+total += match_count(r"XMAS")  # Forward
+total += match_count(r"SAMX")  # Backward
+total += match_count(r"X.{140}M.{140}A.{140}S")  # Vertical
+total += match_count(r"S.{140}A.{140}M.{140}X")  # Upside-down Vertical
+total += match_count(r"X.{141}M.{141}A.{141}S")  # Down-right diagonal
+total += match_count(r"X.{139}M.{139}A.{139}S")  # Down-left diagonal
+total += match_count(r"S.{139}A.{139}M.{139}X")  # Up-right diagonal
+total += match_count(r"S.{141}A.{141}M.{141}X")  # Up-left diagonal
+
+print(total)
---
+++ 2024/Day04/part2.py
@@ -0,0 +1,21 @@
+from re import findall, DOTALL
+
+# Part Two
+# https://adventofcode.com/2024/day/4#part2
+
+file = open("input.txt", "r")
+text = file.read()
+
+
+def match_count(regex: str) -> int:
+    regex = "(?=" + regex + ")"
+    return len(findall(regex, text, DOTALL))
+
+
+total = 0
+total += match_count(r"M.M.{139}A.{139}S.S")  # M's Up
+total += match_count(r"S.S.{139}A.{139}M.M")  # M's Down
+total += match_count(r"M.S.{139}A.{139}M.S")  # M's Left
+total += match_count(r"S.M.{139}A.{139}S.M")  # M's Right
+
+print(total)
